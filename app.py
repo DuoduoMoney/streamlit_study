034d4138ad5541ee480ac57344ce8fd6255d8d82
DuoduoMoney <rocketq147@163.com>
2024-04-22 09:52:01 +0800

    提交信息

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.header('娇娇的AIGC World')
+st.header('娇娇和牛牛的AIGC World')
 st.subheader('First Demo')
 st.text('Description')
-st.markdown('这是 **Markdown** 文本。用来展示图标效果')
+st.markdown('这是 **Markdown** 文本。用来展示图表效果')
 st.latex(r'e^{i\pi} + 1 = 0')
 st.dataframe({'列1': [1, 2, 3], '列2': [4, 5, 6]})
 
-import numpy as np
-import pandas as pd
+
 chart_data = pd.DataFrame(np.random.randn(50, 3), columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
 
